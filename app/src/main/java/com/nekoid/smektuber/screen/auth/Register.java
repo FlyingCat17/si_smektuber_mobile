9f9e3d3f2fae4714973e469e9076e2b7762fbfcb
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-04-10 15:35:02 +0700

    feat: add component and layout auth

--- app/src/main/java/com/nekoid/smektuber/screen/auth/Register.java
+++ app/src/main/java/com/nekoid/smektuber/screen/auth/Register.java
@@ -12,10 +12,10 @@ import com.nekoid.smektuber.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Login#newInstance} factory method to
+ * Use the {@link Register#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Login extends Fragment {
+public class Register extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class Login extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Login() {
+    public Register() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class Login extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Login.
+     * @return A new instance of fragment Register.
      */
     // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
+    public static Register newInstance(String param1, String param2) {
+        Register fragment = new Register();
         Bundle args = new Bundle();
         args.putString( ARG_PARAM1, param1 );
         args.putString( ARG_PARAM2, param2 );
@@ -61,6 +61,6 @@ public class Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_login, container, false );
+        return inflater.inflate( R.layout.fragment_register, container, false );
     }
 }
