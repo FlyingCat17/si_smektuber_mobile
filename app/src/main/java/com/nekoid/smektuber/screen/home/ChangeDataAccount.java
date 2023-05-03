dfbfeecb2b2d05624c7ccfd842e05f9577ca9240
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-03 08:51:51 +0700

    merge main into fahmydev

---
+++ app/src/main/java/com/nekoid/smektuber/screen/home/ChangeDataAccount.java
@@ -0,0 +1,16 @@
+package com.nekoid.smektuber.screen.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nekoid.smektuber.R;
+
+public class ChangeDataAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_data_account);
+    }
+}
--- app/src/main/java/com/nekoid/smektuber/screen/home/HomeMember.java
+++ app/src/main/java/com/nekoid/smektuber/screen/home/HomeMember.java
@@ -1,19 +1,54 @@
 package com.nekoid.smektuber.screen.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.nekoid.smektuber.R;
 import com.nekoid.smektuber.helpers.statusBar.StatusBarUtil;
 
 public class HomeMember extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    Account account = new Account();
+    Dashboard dashboard = new Dashboard();
+
+    NoJobs jobs = new NoJobs();
+    Ppdb ppdb = new Ppdb();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_member);
-        // Panggil method setTransparentStatusBar()
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setItemTextColor(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.r,dashboard).commit();
         StatusBarUtil.setTransparentStatusBar(this);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Dashboard:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,dashboard).commit();
+                        return true;
+                    case R.id.Ppdb:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,ppdb).commit();
+                        return true;
+                    case R.id.Loker:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,jobs).commit();
+                        return true;
+                    case R.id.Akun:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,account).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
