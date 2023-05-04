3a70b4799b4862fb94830a79e83dcccfac223bc3
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-04 20:22:19 +0700

    Add Volley to Hit API on progress)

---
+++ app/src/main/java/com/nekoid/smektuber/config/volley/UrlsApi.java
@@ -0,0 +1,9 @@
+package com.nekoid.smektuber.config.volley;
+
+public class UrlsApi {
+
+    public static final String BASE_URL = "https://lutfisobri.my.id/api/v1";
+//    public static final String HOME = BASE_URL+"api/v1/";
+    public static final String LOGIN = BASE_URL+"auth/login";
+    public static final String REGISTER = BASE_URL+"auth/register";
+}
