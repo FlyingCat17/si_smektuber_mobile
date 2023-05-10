af87633d87a2aacfbe47989a3a26366cf2b3ff77
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-10 15:33:39 +0700

    feat & fix : add register and tidy up folder

---
+++ app/src/main/java/com/nekoid/smektuber/screen/home/ekstarkurikuler/Extrakurikuler.java
@@ -0,0 +1,63 @@
+package com.nekoid.smektuber.screen.home.ekstarkurikuler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.nekoid.smektuber.R;
+import com.nekoid.smektuber.adapter.AdapterDataExtra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Extrakurikuler extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<MenuExtra> list = new ArrayList<>();
+    private AdapterDataExtra adapterDataExtra;
+    @SuppressLint("UseCompatLoadingForDrawables")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extrakurikuler);
+        recyclerView = findViewById(R.id.rvDataExtra);
+
+        list.add(new MenuExtra("Pasukan Pengibar Bendera", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+//        list.add(new MenuExtra("Pramuka", getDrawable(R.drawable.imageuser)));
+
+        adapterDataExtra = new AdapterDataExtra(getApplicationContext(), list);
+        adapterDataExtra.setDialog(new AdapterDataExtra.Dialog() {
+            @Override
+            public void onClick(MenuExtra menuExtra) {
+                Intent x = new Intent(Extrakurikuler.this, DetailExtra.class);
+                startActivity(x);
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapterDataExtra);
+    }
+}
