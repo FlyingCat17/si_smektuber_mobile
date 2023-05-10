3416c779cd2c7d23fcc6417b17bc8ecb077ea165
lutfisobri <lutfisobri@gmail.com>
2023-05-10 16:30:08 +0700

    validate birthday

--- app/src/main/java/com/nekoid/smektuber/screen/home/DaftarPPDB.java
+++ app/src/main/java/com/nekoid/smektuber/screen/home/DaftarPPDB.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -18,6 +20,9 @@ public class DaftarPPDB extends AppCompatActivity {
     private String[] items =  {"TKJ (Teknik Komputer dan Jaringan)","MM (Multi Media)"};
     private AutoCompleteTextView autoCompleteTxt;
     private ArrayAdapter<String> adapterItems;
+
+    private EditText birthDay;
+    private Button register;
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,10 @@ public class DaftarPPDB extends AppCompatActivity {
 
         adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
         autoCompleteTxt.setAdapter(adapterItems);
+
+        birthDay = findViewById(R.id.Dp_Tanggal_Lahir);
+        register = findViewById(R.id.BtnDaftarPPDB);
+        register.setOnClickListener(v -> doRegister());
 
         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,5 +59,40 @@ public class DaftarPPDB extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         Navigator.of(this).pop();
         return true;
+    }
+
+    protected void doRegister() {
+        if (register != null) {
+            if (validateBirthday()) {
+                Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean validateBirthday() {
+        if (birthDay != null) {
+            String day = birthDay.getText().toString();
+            String yearsNow = String.valueOf(java.util.Calendar.getInstance().get(java.util.Calendar.YEAR));
+            if (day.matches("([0-9]{2})/([0-9]{2})/([0-9]{4})")) {
+                String[] split = day.split("/");
+                if (split.length == 3) {
+                    int dayInt = Integer.parseInt(split[0]);
+                    int monthInt = Integer.parseInt(split[1]);
+                    int yearsInt = Integer.parseInt(split[2]);
+
+                    if (yearsInt >= Integer.parseInt(yearsNow) - 17 && yearsInt <= Integer.parseInt(yearsNow) - 13) {
+                        if (monthInt >= 1 && monthInt <= 12) {
+                            if (dayInt >= 1 && dayInt <= 31) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
     }
 }
