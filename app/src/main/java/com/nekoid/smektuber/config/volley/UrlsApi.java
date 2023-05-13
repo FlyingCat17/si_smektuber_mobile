d410b61282fc3ab594c284b33901475f9bcbf3ac
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-13 09:35:38 +0700

    feat: add refresh token and refresh to load data

--- app/src/main/java/com/nekoid/smektuber/config/volley/UrlsApi.java
+++ app/src/main/java/com/nekoid/smektuber/config/volley/UrlsApi.java
@@ -7,6 +7,7 @@ public class UrlsApi {
     public static final String LOGIN = BASE_URL+"/auth/login";
     public static final String REGISTER = BASE_URL+"/auth/register";
     public static final String LOGOUT = BASE_URL+"/auth/logout";
+    public static final String REFRESH_TOKEN = BASE_URL+"/auth/refresh";
     public static final String ABOUT = BASE_URL+"/about";
 
 }
--- app/src/main/java/com/nekoid/smektuber/screen/splash/SplashScreen.java
+++ app/src/main/java/com/nekoid/smektuber/screen/splash/SplashScreen.java
@@ -8,8 +8,16 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.nekoid.smektuber.R;
+import com.nekoid.smektuber.config.volley.UrlsApi;
 import com.nekoid.smektuber.helpers.navigation.Navigator;
 
 import com.nekoid.smektuber.helpers.statusBar.StatusBarUtil;
@@ -17,6 +25,12 @@ import com.nekoid.smektuber.helpers.widget.Style;
 import com.nekoid.smektuber.screen.auth.Login;
 import com.nekoid.smektuber.screen.auth.WelcomeAuth;
 import com.nekoid.smektuber.screen.home.HomeMember;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -44,14 +58,15 @@ public class SplashScreen extends AppCompatActivity {
                 long expiresAt = userPref.getLong("expires_at", 0);
 
                 if (!accessToken.isEmpty() && System.currentTimeMillis() >= expiresAt) {
-                    // Token has expired, perform re-login
-                    Navigator.of(SplashScreen.this).pushReplacement(Login.class);
+                    // Token has expired
+                    //redirectToLogin();
+                    refreshToken();
                 } else {
                     boolean isLoggedIn = userPref.getBoolean("isLoggedIn", false);
                     if (isLoggedIn) {
-                        Navigator.of(SplashScreen.this).pushReplacement(HomeMember.class);
+                        redirectToHome();
                     } else {
-                        Navigator.of(SplashScreen.this).pushReplacement(Login.class);
+                        redirectToLogin();
                     }
                 }
             }
@@ -65,4 +80,64 @@ public class SplashScreen extends AppCompatActivity {
         logo.startAnimation(anim);
 
     }
+    private void redirectToLogin(){
+        Navigator.of(SplashScreen.this).pushReplacement(Login.class);
+    }
+    private void redirectToHome(){
+        Navigator.of(SplashScreen.this).pushReplacement(HomeMember.class);
+    }
+    private void refreshToken() {
+        String url = UrlsApi.REFRESH_TOKEN;
+        SharedPreferences userPref = getSharedPreferences("user", MODE_PRIVATE);
+        String refreshToken = userPref.getString("refresh_token", "");
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                if (jsonObject.getInt("status") == 200) {
+                    String newAccessToken = jsonObject.getJSONObject("data").getString("access_token");
+                    int expiresIn = jsonObject.getJSONObject("data").getInt("expires_in");
+
+                    SharedPreferences.Editor editor = userPref.edit();
+                    editor.putString("access_token", newAccessToken);
+                    editor.putLong("expires_at", System.currentTimeMillis() + (expiresIn * 1000));
+                    editor.apply();
+
+                    redirectToHome();
+                    Toast.makeText( SplashScreen.this, "Test : Access_Token Refresh", Toast.LENGTH_SHORT ).show();
+                } else {
+                    redirectToLogin();
+                    Toast.makeText( SplashScreen.this, "Test : Token Fail to refresh", Toast.LENGTH_SHORT ).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            error.printStackTrace();
+            redirectToLogin();
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("refresh_token", refreshToken);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                String accessToken = userPref.getString("access_token", "");
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + accessToken);
+                headers.putAll(super.getHeaders());
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        queue.add(request);
+    }
+
 }
