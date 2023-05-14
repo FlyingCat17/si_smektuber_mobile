49b53ed5b3a8b7592af12ffbbf8c8481ad370797
lutfisobri <lutfisobri@gmail.com>
2023-05-14 20:33:19 +0700

    fix: usermodel

--- app/src/main/java/com/nekoid/smektuber/models/UserModel.java
+++ app/src/main/java/com/nekoid/smektuber/models/UserModel.java
@@ -6,16 +6,22 @@ import org.json.JSONObject;
 
 public class UserModel extends Models {
     public String name;
+
     public String email;
+
     public String role;
+
     public String username;
+
+    public String avatar;
 
     public boolean emailVerified;
 
-    public UserModel(String name, String email, String role, String username, boolean emailVerified) {
+    public UserModel(String name, String email, String role, String avatar, String username, boolean emailVerified) {
         this.name = name;
         this.email = email;
         this.role = role;
+        this.avatar = avatar;
         this.username = username;
         this.emailVerified = emailVerified;
     }
@@ -26,6 +32,7 @@ public class UserModel extends Models {
                     json.getString("name"),
                     json.getString("email"),
                     json.getString("role"),
+                    json.getString("avatar"),
                     json.getString("username"),
                     json.getBoolean("email_verified_at")
             );
@@ -33,30 +40,5 @@ public class UserModel extends Models {
             e.printStackTrace();
             return null;
         }
-    }
-
-    @Override
-    public String toJson() {
-        String json = "{";
-        json += "\"name\":\"" + name + "\",";
-        json += "\"email\":\"" + email + "\",";
-        json += "\"role\":\"" + role + "\",";
-        json += "\"username\":\"" + username + "\"";
-        json += "}";
-        return json;
-    }
-
-    @NonNull
-    @Override
-    public String toString() {
-        String user = "name:";
-        user += name;
-        user += ", email:";
-        user += email;
-        user += ", role:";
-        user += role;
-        user += ", username:";
-        user += username;
-        return user;
     }
 }
