287c6949fce9d575cbb2fede7d5ae9d7cd4f10c5
lutfisobri <lutfisobri@gmail.com>
2023-05-14 20:34:13 +0700

    feat: update account

--- app/src/main/java/com/nekoid/smektuber/screen/home/account/ChangeDataAccount.java
+++ app/src/main/java/com/nekoid/smektuber/screen/home/account/ChangeDataAccount.java
@@ -1,59 +1,74 @@
 package com.nekoid.smektuber.screen.home.account;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.nekoid.smektuber.R;
+import com.nekoid.smektuber.config.volley.Endpoint;
+import com.nekoid.smektuber.config.volley.PublicApi;
 import com.nekoid.smektuber.helpers.navigation.Navigator;
-import com.nekoid.smektuber.screen.auth.Register;
+import com.nekoid.smektuber.helpers.utils.BaseActivity;
+import com.nekoid.smektuber.helpers.utils.LocalStorage;
+import com.nekoid.smektuber.helpers.utils.TextChangeListerner;
+import com.nekoid.smektuber.models.UserModel;
+import com.nekoid.smektuber.network.Http;
+import com.nekoid.smektuber.network.Response;
+import com.squareup.picasso.Picasso;
 
-public class ChangeDataAccount extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChangeDataAccount extends BaseActivity {
+
     ImageView iconUpload, ImageUser;
-//    Button Back;
+
     Toolbar toolbar;
+
+    TextInputLayout layoutCaUsername, layoutCaFullName, layoutCaEmail, layoutCaPassword, layoutCaConfirmPassword;
+
+    TextInputEditText caUsername, caFullName, caEmail, caPassword, caConfirmPassword;
+
+    Button btnUpdate;
+
+    private UserModel userModel;
+
+    private boolean isUpdateAvatar = false;
+
+    private Uri uri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_data_account);
-        iconUpload = findViewById(R.id.IconUploadImage);
-        ImageUser = findViewById(R.id.ImageUserProfil);
 
-        iconUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImagePicker.with(ChangeDataAccount.this)
-                        .crop()	    			//Crop image(Optional), Check Customization for more option
-                        .cropSquare()
-                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
-                        .start();
-            }
-        });
-//        Back = findViewById(R.id.backIcon);
-//
-//        Back.setOnClickListener( v->{
-//            Navigator.push( this, Register.class );
-//        } );
+        setVariable();
         setToolbar();
+        onListener();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
-            Uri uri = data.getData();
+            uri = data.getData();
             ImageUser.setImageURI(uri);
+            isUpdateAvatar = uri != null;
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
             Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
         } else {
@@ -61,16 +76,257 @@ public class ChangeDataAccount extends AppCompatActivity {
         }
     }
 
-
-
-    private void setToolbar(){
-        toolbar = findViewById( R.id.backIcon );
+    private void setToolbar() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         Navigator.of(this).pop();
         return true;
+    }
+
+    private void setVariable() {
+        // User Model
+        userModel = LocalStorage.userModel;
+
+        // toolbar
+        toolbar = findViewById(R.id.backIcon);
+
+        // image
+        iconUpload = findViewById(R.id.IconUploadImage);
+        ImageUser = findViewById(R.id.ImageUserProfil);
+
+        // layout for input text to handle error
+        layoutCaUsername = findViewById(R.id.layoutCaUsername);
+        layoutCaFullName = findViewById(R.id.layoutCaFullName);
+        layoutCaEmail = findViewById(R.id.layoutCaEmail);
+        layoutCaPassword = findViewById(R.id.layoutCaPassword);
+        layoutCaConfirmPassword = findViewById(R.id.layoutCaConfirmPassword);
+
+        // input text
+        caUsername = findViewById(R.id.Ca_username);
+        caFullName = findViewById(R.id.Ca_NameLengkap);
+        caEmail = findViewById(R.id.Ca_Email);
+        caPassword = findViewById(R.id.Ca_password);
+        caConfirmPassword = findViewById(R.id.Ca_KonfirmasiPassword);
+
+        // button
+        btnUpdate = findViewById(R.id.BtnMemperbarui);
+
+        setModelToView();
+    }
+
+    // set model to view
+    private void setModelToView() {
+        if (userModel != null) {
+            caUsername.setText(userModel.username);
+            caFullName.setText(userModel.name);
+            caEmail.setText(userModel.email);
+            if (userModel.avatar != null && !userModel.avatar.isEmpty()) {
+                Picasso.get().load(userModel.avatar).into(ImageUser);
+            }
+        }
+    }
+
+    private String username() {
+        return caUsername.getText().toString();
+    }
+
+    private String fullName() {
+        return caFullName.getText().toString();
+    }
+
+    private String email() {
+        return caEmail.getText().toString();
+    }
+
+    private String password() {
+        return caPassword.getText().toString();
+    }
+
+    private String confirmPassword() {
+        return caConfirmPassword.getText().toString();
+    }
+
+    private Map<String, String> getUpdateAccount() {
+        Map<String, String> updateAccount = new HashMap<>();
+        updateAccount.put("username", username());
+        updateAccount.put("name", fullName());
+        updateAccount.put("email", email());
+        return updateAccount;
+    }
+
+    public Map<String, String> getUpdatePassword() {
+        Map<String, String> updatePassword = new HashMap<>();
+        updatePassword.put("old_password", getCredentials());
+        updatePassword.put("password", password());
+        updatePassword.put("password_confirmation", confirmPassword());
+        return updatePassword;
+    }
+
+    private Map<String, String> getUpdateAvatar() {
+        Map<String, String> updateAvatar = new HashMap<>();
+        updateAvatar.put("avatar", uri.toString());
+        return updateAvatar;
+    }
+
+    /**
+     * <p>Handle Error</p>
+     *
+     * <p>We can custom message if error detection</p>
+     */
+    public boolean validator() {
+        String username = username();
+        String fullName = fullName();
+
+        if (username.isEmpty()) {
+            layoutCaUsername.setErrorEnabled(true);
+            layoutCaUsername.setError("username tidak boleh kosing");
+            return false;
+        }
+
+        if (fullName.isEmpty()) {
+            layoutCaFullName.setErrorEnabled(true);
+            layoutCaFullName.setError("Nama lengkap tidak boleh kosong");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void onListener() {
+        iconUpload.setOnClickListener(v -> {
+            ImagePicker.with(this)
+                    .crop()                    //Crop image(Optional), Check Customization for more option
+                    .cropSquare()
+                    .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                    .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                    .saveDir(getExternalCacheDir())
+                    .start();
+        });
+
+        btnUpdate.setOnClickListener(v -> {
+            if (!validator()) return;
+            if (!emailValidator(layoutCaEmail, caEmail)) return;
+
+            // add request for update account
+            doAccountUpdate();
+
+            // user change avatar
+            if (isUpdateAvatar) {
+                doUpdateAvatar();
+                isUpdateAvatar = false;
+            }
+
+            // user want to change password
+            if (isChangePassword()) {
+                doChangePassword();
+            }
+
+            resetViewPassword();
+        });
+
+        caUsername.addTextChangedListener(new TextChangeListerner() {
+            @Override
+            protected void onTextChanged(String before, String old, String aNew, String after) {
+                if (!username().isEmpty()) {
+                    layoutCaUsername.setErrorEnabled(false);
+                }
+            }
+        });
+
+        caFullName.addTextChangedListener(new TextChangeListerner() {
+            @Override
+            protected void onTextChanged(String before, String old, String aNew, String after) {
+                if (!fullName().isEmpty()) {
+                    layoutCaFullName.setErrorEnabled(false);
+                }
+            }
+        });
+
+        caEmail.addTextChangedListener(new TextChangeListerner() {
+            @Override
+            protected void onTextChanged(String before, String old, String aNew, String after) {
+                if (!email().isEmpty()) {
+                    layoutCaEmail.setErrorEnabled(false);
+                }
+            }
+        });
+    }
+
+    private void resetViewPassword() {
+        caPassword.setText("");
+        caConfirmPassword.setText("");
+    }
+
+    private Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + getToken());
+        return headers;
+    }
+
+    private void doAccountUpdate() {
+        try {
+            Response response = Http.put(Endpoint.UPDATE_USER.getUrl(), getHeaders(), getUpdateAccount());
+            JSONObject body = new JSONObject(response.body.toString());
+            if (response.statusCode != 200) {
+                Toast.makeText(this, body.getString("message"), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            userModel = UserModel.fromJson(body.getJSONObject("data"));
+            LocalStorage.setUserModel(userModel);
+            setModelToView();
+
+            // you can add more action after update account
+            Toast.makeText(this, "Berhasil memperbarui akun", Toast.LENGTH_SHORT).show();
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doChangePassword() {
+        try {
+            Response response = Http.put(Endpoint.UPDATE_PASSWORD.getUrl(), getHeaders(), getUpdatePassword());
+            JSONObject body = new JSONObject(response.body.toString());
+            if (response.statusCode != 202) {
+                Toast.makeText(this, body.getString("message"), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            doLogin(username(), password());
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doUpdateAvatar() {
+        try {
+            Map<String, File> body = new HashMap<>();
+            body.put("avatar", new File(getExternalCacheDir(), new File(this.uri.toString()).getName()));
+
+            Response response = Http.multipartFile(Endpoint.UPDATE_AVATAR.getUrl(), body, getHeaders());
+
+            if (response.statusCode != 200) {
+                System.out.println(response.body);
+                Toast.makeText(this, "engror" + response.statusCode, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            JSONObject responseBody = new JSONObject(response.body.toString());
+
+            userModel = UserModel.fromJson(responseBody.getJSONObject("data"));
+            LocalStorage.setUserModel(userModel);
+            setModelToView();
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isChangePassword() {
+        if (!password().isEmpty() && !confirmPassword().isEmpty()) {
+            return password().equals(confirmPassword());
+        }
+        return false;
     }
 }
