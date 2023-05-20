9d4f06b05ce655f5cf566a66054511c4ef0bf4da
lutfisobri <lutfisobri@gmail.com>
2023-05-20 21:17:52 +0700

    update account, splash screen, visi misi

--- app/src/main/java/com/nekoid/smektuber/screen/splash/SplashScreen.java
+++ app/src/main/java/com/nekoid/smektuber/screen/splash/SplashScreen.java
@@ -9,11 +9,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nekoid.smektuber.R;
+import com.nekoid.smektuber.api.Endpoint;
+import com.nekoid.smektuber.api.PublicApi;
 import com.nekoid.smektuber.helpers.navigation.Navigator;
 
 import com.nekoid.smektuber.helpers.statusBar.StatusBarUtil;
 import com.nekoid.smektuber.helpers.utils.AnimationListener;
 import com.nekoid.smektuber.helpers.utils.BaseActivity;
+import com.nekoid.smektuber.helpers.utils.State;
+import com.nekoid.smektuber.network.Http;
 import com.nekoid.smektuber.screen.auth.Login;
 import com.nekoid.smektuber.screen.home.HomeMember;
 
@@ -29,6 +33,7 @@ public class SplashScreen extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        deleteCache();
         setContentView(R.layout.activity_splash_screen);
         txtSplashVersion = findViewById(R.id.splashVersion);
         getVersion();
